19d44d1c8b0696061f6d0b3820d4206256755a9c
Maciej Patelczyk <maciej.patelczyk@intel.com>
2012-12-12 15:01:01 +0100

    Release 0.70 (sourceforge.net)
    Signed-off-by: Maciej Patelczyk <maciej.patelczyk@intel.com>

--- src/version.h
+++ src/version.h
@@ -36,7 +36,7 @@
  * in makefile to reflect current version number.
  */
 #ifndef VERSION_MINOR
-#define VERSION_MINOR    40
+#define VERSION_MINOR    70
 #endif
 
 #endif /* _VERSION_H_INCLUDED_ */

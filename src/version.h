f0e267272b331ee3fe82fb2ee902ac23f4419f2f
Lukasz Dorau <lukasz.dorau@intel.com>
2013-02-12 12:42:13 +0000

    Release 0.77
    Signed-off-by: Lukasz Dorau <lukasz.dorau@intel.com>

--- src/version.h
+++ src/version.h
@@ -33,7 +33,7 @@
  * in makefile to reflect current version number.
  */
 #ifndef VERSION_MINOR
-#define VERSION_MINOR    76
+#define VERSION_MINOR    77
 #endif
 
 #endif				/* _VERSION_H_INCLUDED_ */

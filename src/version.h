be5c7bf743b421a21be93791c40dd4ca91c87315
Pawel Baldysiak <pawel.baldysiak@intel.com>
2016-10-28 14:45:41 +0200

    Release 0.80
    Signed-off-by: Pawel Baldysiak <pawel.baldysiak@intel.com>

--- src/version.h
+++ src/version.h
@@ -33,7 +33,7 @@
  * in makefile to reflect current version number.
  */
 #ifndef VERSION_MINOR
-#define VERSION_MINOR    79
+#define VERSION_MINOR    80
 #endif
 
 #endif				/* _VERSION_H_INCLUDED_ */

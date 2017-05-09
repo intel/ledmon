intel/ledmon

commit b30173ec8c05c2e9517c512d6cf7df9d9d3d4e45

* fix off-normal-failure block dev status loop * remove unused raid->slave_list member * support md raid on block device partitions

src/slave.c
@@ -100,13 +100,26 @@ static struct block_device *_get_block(const char *path, void *block_list)
 {
 	char temp[PATH_MAX];
 	char link[PATH_MAX];
+	char *ptr;
 	struct block_device *device = NULL;
 
 	str_cpy(temp, path, PATH_MAX);
 	str_cat(temp, "/block", PATH_MAX);
 
-	if (realpath(temp, link))
-		device = list_first_that(block_list, _compare, link);
+	if (realpath(temp, link)) {
+		ptr = strrchr(link, '/');
+		if (ptr && link < ptr - strlen("/block")) {
+			/* translate partition to master block dev */
+			if(strncmp(
+				ptr - strlen("/block"),
+				"/block",
+				strlen("/block"))) {
+
+				*ptr = '\0';
+			}
+			device = list_first_that(block_list, _compare, link);
+		}
+	}
 	return device;
 }
 

src/ledmon.c
@@ -595,7 +595,7 @@ static void _add_block(struct block_device *block)
 				temp->ibpi = block->ibpi;
 			}
 		} else {
-			temp->ibpi = IBPI_PATTERN_ONESHOT_NORMAL;
+			temp->ibpi = block->ibpi;
 		}
 
 		if (ibpi != temp->ibpi) {

src/raid.c
@@ -141,7 +141,6 @@ struct raid_device *raid_device_init(const char *path, unsigned int device_num,
 			device->sync_action = _get_sync_action(path);
 			device->array_state = state;
 			device->level = _get_level(path);
-			device->slave_list = NULL;
 			device->degraded = get_int(path, -1, "md/degraded");
 			device->raid_disks = get_int(path, 0, "md/raid_disks");
 			device->type = type;

src/raid.h
@@ -77,7 +77,6 @@ struct raid_device {
 	int device_num;
 	char *sysfs_path;
 	int raid_disks;
-	void *slave_list;
 	int degraded;
 	enum raid_state array_state;
 	enum raid_action sync_action;

commit 868a01e894f6abd33a98457a7ee4c1aa7e1eb2fe

Don't rely on searching for "/block" in sysfs path for detecting partitions
It is simpler and more reliable to check for a "partition" file in the
device's directory (supported since kernel 2.6.28). The previous
approach would fail for nvme devices because their device path looks
like this:
/sys/devices/pci0000:00/0000:00:0c.0/nvme/nvme0/nvme0n1

Fixes: b30173ec8c05 ("* fix off-normal-failure block dev status loop * remove unused raid->slave_list member * support md raid on block device partitions")

src/slave.c
@@ -22,6 +22,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <sys/stat.h>
 
 #if _HAVE_DMALLOC_H
 #include <dmalloc.h>
@@ -100,27 +102,26 @@ static struct block_device *_get_block(const char *path, void *block_list)
 {
 	char temp[PATH_MAX];
 	char link[PATH_MAX];
-	char *ptr;
-	struct block_device *device = NULL;
 
 	str_cpy(temp, path, PATH_MAX);
 	str_cat(temp, "/block", PATH_MAX);
 
-	if (realpath(temp, link)) {
-		ptr = strrchr(link, '/');
-		if (ptr && link < ptr - strlen("/block")) {
-			/* translate partition to master block dev */
-			if(strncmp(
-				ptr - strlen("/block"),
-				"/block",
-				strlen("/block"))) {
+	if (!realpath(temp, link))
+		return NULL;
 
+	/* translate partition to master block dev */
+	if (snprintf(temp, PATH_MAX, "%s/partition", link) > 0) {
+		struct stat sb;
+		char *ptr;
+
+		if (stat(temp, &sb) == 0 && S_ISREG(sb.st_mode)) {
+			ptr = strrchr(link, '/');
+			if (ptr)
 				*ptr = '\0';
-			}
-			device = list_first_that(block_list, _compare, link);
 		}
 	}
-	return device;
+
+	return list_first_that(block_list, _compare, link);
 }
 
 /**
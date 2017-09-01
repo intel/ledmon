intel/ledmon

commit 1de67a0cd44939d367e0684aed73ec90c6a22bae

add support for SGPIO via SMP GPIO_REG_TYPE_TX register
Signed-off-by: Artur Paszkiewicz <artur.paszkiewicz@intel.com>

src/smp.c
@@ -51,36 +51,37 @@
 
 #define GPIO_TX_GP1	0x01
 
-struct gpio_tx_register_byte {
-	unsigned char activity:3;
-	unsigned char locate:2;
-	unsigned char error:3;
-} __attribute__ ((__packed__));
-
-struct gpio_tx_register_byte gpio_tx_table[4];
-
 #define INIT_IBPI(act, loc, err)  \
 	{	.error = err,     \
 		.locate = loc,    \
 		.activity = act   \
 	}
 
+#define LED_OFF	0
+#define LED_ON	1
+#define LED_4HZ	2
+#define LED_I4HZ	3
+#define LED_SOF	4
+#define LED_EOF	5
+#define LED_2HZ	6
+#define LED_I2HZ	7
+
 static const struct gpio_rx_table {
 	struct gpio_tx_register_byte pattern;
 	int support_mask;
 } ibpi2sgpio[] = {
-	[IBPI_PATTERN_UNKNOWN]        = { INIT_IBPI(0,0,0), 1 }, /* OK */
-	[IBPI_PATTERN_ONESHOT_NORMAL] = { INIT_IBPI(0,0,0), 1 }, /* OK */
-	[IBPI_PATTERN_NORMAL]         = { INIT_IBPI(0,0,0), 1 }, /* OK */
-	[IBPI_PATTERN_DEGRADED]       = { INIT_IBPI(0,0,0), 0 }, /* NO */
-	[IBPI_PATTERN_REBUILD]        = { INIT_IBPI(0,1,1), 1 }, /* OK */
-	[IBPI_PATTERN_REBUILD_P]      = { INIT_IBPI(0,0,0), 0 }, /* NO */
-	[IBPI_PATTERN_FAILED_ARRAY]   = { INIT_IBPI(0,0,0), 0 }, /* NO */
-	[IBPI_PATTERN_HOTSPARE]       = { INIT_IBPI(0,0,0), 0 }, /* NO */
-	[IBPI_PATTERN_PFA]            = { INIT_IBPI(0,0,0), 0 }, /* NO */
-	[IBPI_PATTERN_FAILED_DRIVE]   = { INIT_IBPI(0,0,1), 1 }, /* OK */
-	[IBPI_PATTERN_LOCATE]         = { INIT_IBPI(0,1,0), 1 }, /* OK */
-	[IBPI_PATTERN_LOCATE_OFF]     = { INIT_IBPI(0,0,0), 1 }  /* OK */
+	[IBPI_PATTERN_UNKNOWN]        = { INIT_IBPI(LED_SOF,LED_OFF,LED_OFF), 1 }, /* OK */
+	[IBPI_PATTERN_ONESHOT_NORMAL] = { INIT_IBPI(LED_SOF,LED_OFF,LED_OFF), 1 }, /* OK */
+	[IBPI_PATTERN_NORMAL]         = { INIT_IBPI(LED_SOF,LED_OFF,LED_OFF), 1 }, /* OK */
+	[IBPI_PATTERN_DEGRADED]       = { INIT_IBPI(LED_SOF,LED_OFF,LED_OFF), 0 }, /* NO */
+	[IBPI_PATTERN_REBUILD]        = { INIT_IBPI(LED_SOF,LED_ON,LED_ON), 1 }, /* OK */
+	[IBPI_PATTERN_REBUILD_P]      = { INIT_IBPI(LED_SOF,LED_OFF,LED_OFF), 0 }, /* NO */
+	[IBPI_PATTERN_FAILED_ARRAY]   = { INIT_IBPI(LED_SOF,LED_4HZ,LED_OFF), 0 }, /* NO */
+	[IBPI_PATTERN_HOTSPARE]       = { INIT_IBPI(LED_SOF,LED_OFF,LED_4HZ), 0 }, /* NO */
+	[IBPI_PATTERN_PFA]            = { INIT_IBPI(LED_SOF,LED_OFF,LED_2HZ), 0 }, /* NO */
+	[IBPI_PATTERN_FAILED_DRIVE]   = { INIT_IBPI(LED_SOF,LED_OFF,LED_ON), 1 }, /* OK */
+	[IBPI_PATTERN_LOCATE]         = { INIT_IBPI(LED_SOF,LED_ON,LED_OFF), 1 }, /* OK */
+	[IBPI_PATTERN_LOCATE_OFF]     = { INIT_IBPI(LED_SOF,LED_OFF,LED_OFF), 1 }  /* OK */
 };
 
 struct smp_read_response_frame_header {
@@ -244,7 +245,7 @@ int set_raw_pattern(unsigned int dev_idx, unsigned char *data,
 	int od_offset = dev_idx * 3;
 	int rc = 0;
 
-	if (pattern->activity)
+	if (pattern->activity == LED_ON)
 		rc +=
 		    try_set_sas_gpio_gp_bit(od_offset + 0, data, GPIO_TX_GP1,
 					    1);
@@ -253,7 +254,7 @@ int set_raw_pattern(unsigned int dev_idx, unsigned char *data,
 		    try_clear_sas_gpio_gp_bit(od_offset + 0, data, GPIO_TX_GP1,
 					      1);
 
-	if (pattern->locate)
+	if (pattern->locate == LED_ON)
 		rc +=
 		    try_set_sas_gpio_gp_bit(od_offset + 1, data, GPIO_TX_GP1,
 					    1);
@@ -262,7 +263,7 @@ int set_raw_pattern(unsigned int dev_idx, unsigned char *data,
 		    try_clear_sas_gpio_gp_bit(od_offset + 1, data, GPIO_TX_GP1,
 					      1);
 
-	if (pattern->error)
+	if (pattern->error == LED_ON)
 		rc +=
 		    try_set_sas_gpio_gp_bit(od_offset + 2, data, GPIO_TX_GP1,
 					    1);
@@ -314,40 +315,6 @@ static int _close_smp_device(int fd)
 {
 	return close(fd);
 }
-
-/* smp constants */
-#define SMP_FRAME_TYPE_REQ	0x40
-#define SMP_FRAME_TYPE_RESP	0x41
-
-#define SMP_FUNC_GPIO_READ	0x02
-#define SMP_FUNC_GPIO_WRITE	0x82
-
-#define SMP_FRAME_CRC_LEN		sizeof(uint32_t)
-#define SMP_DATA_CHUNK_SIZE	sizeof(uint32_t)
-
-/* gpio constants */
-/* gpio register types */
-#define GPIO_REG_TYPE_CFG		0x00
-#define GPIO_REG_TYPE_RX		0x01
-#define GPIO_REG_TYPE_RX_GP	0x02
-#define GPIO_REG_TYPE_TX		0x03
-#define GPIO_REG_TYPE_TX_GP	0x04
-
-/* gpio register indexes */
-#define GPIO_REG_IND_CFG_0	0x00
-#define GPIO_REG_IND_CFG_1	0x01
-
-#define GPIO_REG_IND_RX_0		0x00
-#define GPIO_REG_IND_RX_1		0x01
-
-#define GPIO_REG_IND_TX_0		0x00
-#define GPIO_REG_IND_TX_1		0x01
-
-#define SG_RESPONSE_TIMEOUT (5 * 1000)	/* 1000 as miliseconds multiplier */
-#define SCSI_MAX_CDB_LENGTH	0x10
-
-#define GPIO_STATUS_OK			0x00
-#define GPIO_STATUS_FAILURE 0x80
 
 /**
    @brief use sg protocol in order to send data directly to hba driver
@@ -437,7 +404,7 @@ static int _start_smp_write_gpio(int hba,
 /**
    @brief prepare smp frame header
  */
-static int _smp_write_gpio(const char *path, int smp_reg_type,
+int smp_write_gpio(const char *path, int smp_reg_type,
 			   int smp_reg_index, int smp_reg_count, void *data,
 			   size_t len)
 {
@@ -462,8 +429,6 @@ static int _smp_write_gpio(const char *path, int smp_reg_type,
 #define DEFAULT_MAXIMUM_ACTIVITY_ON		2
 #define DEFAULT_STRETCH_ACTIVITY_OFF		0
 #define DEFAULT_STRETCH_ACTIVITY_ON		0
-
-#define DEFAULT_ISCI_SUPPORTED_DEVS		4
 
 /* one data chunk is 32bit long */
 #define SMP_DATA_CHUNKS				1
@@ -494,16 +459,12 @@ int scsi_smp_fill_buffer(struct block_device *device, enum ibpi_pattern ibpi)
 		log_debug("No SCSI ctrl dev '%s'", strstr(sysfs_path, "host"));
 		__set_errno_and_return(EINVAL);
 	}
-	if (!device->cntrl->isci_present) {
-		log_debug("No ISCI ctrl for '%s'", strstr(sysfs_path, "host"));
-		__set_errno_and_return(ENODEV);
-	}
 	if (!device->host) {
 		log_debug("No host for '%s'", strstr(sysfs_path, "host"));
 		__set_errno_and_return(ENODEV);
 	}
 
-	if (!ibpi2sgpio[ibpi].support_mask) {
+	if (device->cntrl->isci_present && !ibpi2sgpio[ibpi].support_mask) {
 		char *c = strrchr(device->sysfs_path, '/');
 		if (c++) {
 			log_debug
@@ -528,9 +489,14 @@ int scsi_smp_fill_buffer(struct block_device *device, enum ibpi_pattern ibpi)
 		__set_errno_and_return(ENODEV);
 	}
 
-	/* update bit stream for this device */
-	set_raw_pattern(device->phy_index,
+	if (device->cntrl->isci_present) {
+		/* update bit stream for this device */
+		set_raw_pattern(device->phy_index,
 			&device->host->bitstream[0], &ibpi2sgpio[ibpi].pattern);
+	} else {
+		device->host->ibpi_state_buffer[device->phy_index] =
+			ibpi2sgpio[ibpi].pattern;
+	}
 
 	/* write only if state has changed */
 	if (ibpi != device->ibpi_prev)
@@ -551,24 +517,30 @@ int scsi_smp_write_buffer(struct block_device *device)
 	if (device->host->flush) {
 		device->host->flush = 0;
 		/* re-transmit the bitstream */
-		return _smp_write_gpio(sysfs_path,
-		                       GPIO_REG_TYPE_TX_GP,
-		                       GPIO_TX_GP1, 1,
-		                       &device->host->bitstream[0],
-		                       SMP_DATA_CHUNKS);
+		if (device->cntrl->isci_present) {
+			return smp_write_gpio(sysfs_path,
+					       GPIO_REG_TYPE_TX_GP,
+					       GPIO_TX_GP1, 1,
+					       &device->host->bitstream[0],
+					       SMP_DATA_CHUNKS);
+		} else {
+			return smp_write_gpio(sysfs_path,
+					       GPIO_REG_TYPE_TX,
+					       0, (device->host->ports+3)/4,
+					       device->host->ibpi_state_buffer,
+					       (device->host->ports+3)/4);
+		}
 	} else
 		return 1;
 }
 
 /**
  */
-void init_smp(const char *path, struct cntrl_device *device)
+static void init_smp(const char *path, struct cntrl_device *device)
 {
 	struct _host_type *hosts;
 	int i;
 	if (!device)
-		return;
-	if (!device->isci_present)
 		return;
 
 	for (hosts = device->hosts; hosts; hosts = hosts->next) {
@@ -576,14 +548,14 @@ void init_smp(const char *path, struct cntrl_device *device)
 		if (hosts->ibpi_state_buffer)
 			continue;
 		hosts->ibpi_state_buffer =
-				calloc(DEFAULT_ISCI_SUPPORTED_DEVS,
+				calloc(hosts->ports,
 					sizeof(struct
 					gpio_tx_register_byte));
 
 		if (!hosts->ibpi_state_buffer)
 			continue;
 
-		for (i = 0; i < DEFAULT_ISCI_SUPPORTED_DEVS; i++)
+		for (i = 0; i < hosts->ports; i++)
 			set_raw_pattern(i, &hosts->bitstream[0],
 					&ibpi2sgpio
 					[IBPI_PATTERN_ONESHOT_NORMAL].pattern);
@@ -593,7 +565,7 @@ void init_smp(const char *path, struct cntrl_device *device)
 
 /**
  */
-int isci_cntrl_init_smp(const char *path, struct cntrl_device *cntrl)
+int cntrl_init_smp(const char *path, struct cntrl_device *cntrl)
 {
 	char *path2 = NULL;
 	char *c;
@@ -602,9 +574,6 @@ int isci_cntrl_init_smp(const char *path, struct cntrl_device *cntrl)
 	DIR *d;
 
 	if (!cntrl)
-		return port;
-
-	if (!cntrl->isci_present)
 		return port;
 
 	/* Other case - just init controller. */

src/block.c
@@ -247,22 +247,15 @@ struct block_device *block_device_init(void *cntrl_list, const char *path)
 				hosts = hosts->next;
 			}
 			if (cntrl->cntrl_type == CNTRL_TYPE_SCSI) {
-				if (dev_directly_attached(link))
-					device->phy_index =
-					    isci_cntrl_init_smp(link, cntrl);
-				else {
-					device->phy_index =
-					    isci_cntrl_init_smp(link, cntrl);
-					if (scsi_get_enclosure(device) == 0) {
-						log_warning("Device " \
-							    "initialization " \
-							    "failed for '%s'",
-									path);
-						free(device->sysfs_path);
-						free(device->cntrl_path);
-						free(device);
-						device = NULL;
-					}
+				device->phy_index = cntrl_init_smp(link, cntrl);
+				if (!dev_directly_attached(link)
+						&& !scsi_get_enclosure(device)) {
+					log_warning("Device initialization failed for '%s'",
+							path);
+					free(device->sysfs_path);
+					free(device->cntrl_path);
+					free(device);
+					device = NULL;
 				}
 			}
 		} else

src/cntrl.c
@@ -35,25 +35,13 @@
 #include "cntrl.h"
 #include "utils.h"
 #include "sysfs.h"
-
-enum int_cntrl_type {
-	INT_CNTRL_TYPE_UNKNOWN = CNTRL_TYPE_UNKNOWN,
-	INT_CNTRL_TYPE_SCSI    = CNTRL_TYPE_SCSI,
-	INT_CNTRL_TYPE_AHCI    = CNTRL_TYPE_AHCI,
-	INT_CNTRL_TYPE_ISCI    = CNTRL_TYPE_AHCI + 1,
-	INT_CNTRL_TYPE_DELLSSD = CNTRL_TYPE_DELLSSD,
-};
+#include "smp.h"
 
 /**
  */
-static int _is_scsi_cntrl(const char *path)
+static int _is_storage_controller(const char *path)
 {
-	uint64_t class;
-
-	if (sysfs_enclosure_attached_to_cntrl(path) == 0)
-		return 0;
-
-	class = get_uint64(path, 0, "class");
+	uint64_t class = get_uint64(path, 0, "class");
 	return (class & 0xff0000L) == 0x010000L;
 }
 
@@ -61,13 +49,7 @@ static int _is_scsi_cntrl(const char *path)
  */
 static int _is_isci_cntrl(const char *path)
 {
-	uint64_t class;
-
-	if (sysfs_isci_driver(path) == 0)
-		return 0;
-
-	class = get_uint64(path, 0, "class");
-	return (class & 0xff0000L) == 0x010000L;
+	return sysfs_isci_driver(path);
 }
 
 /**
@@ -103,6 +85,40 @@ static int _is_dellssd_cntrl(const char *path)
 }
 
 /**
+ */
+static int _is_smp_cntrl(const char *path)
+{
+	int result = 0;
+	void *dir = scan_dir(path);
+	char *p;
+	char host_path[PATH_MAX] = { 0 };
+	char *host;
+	if (dir) {
+		host = list_head(dir);
+		while (host) {
+			p = strrchr(host, '/');
+			if (!p++)
+				break;
+			if (strncmp(p, "host", strlen("host")) == 0) {
+				snprintf(host_path, sizeof(host_path),
+					"%s/%s/bsg/sas_%s", path, p, p);
+				result = smp_write_gpio(host_path,
+					GPIO_REG_TYPE_TX,
+					0,
+					0,
+					"",
+					0) == 0;
+			}
+			host = list_next(host);
+		}
+		list_fini(dir);
+	}
+
+	return result;
+}
+
+
+/**
  * @brief Determines the type of controller.
  *
  * This is internal function of 'controller device' module. The function
@@ -115,18 +131,20 @@ static int _is_dellssd_cntrl(const char *path)
  *         CNTRL_TYPE_UNKNOWN this means a controller device is not
  *         supported.
  */
-static enum int_cntrl_type _get_type(const char *path)
+static enum cntrl_type _get_type(const char *path)
 {
-	enum int_cntrl_type type = CNTRL_TYPE_UNKNOWN;
+	enum cntrl_type type = CNTRL_TYPE_UNKNOWN;
 
-	if (_is_dellssd_cntrl(path))
+	if (_is_dellssd_cntrl(path)) {
 		type = CNTRL_TYPE_DELLSSD;
-	if (_is_scsi_cntrl(path))
-		type = CNTRL_TYPE_SCSI;
-	if (_is_isci_cntrl(path))
-		type = INT_CNTRL_TYPE_ISCI;
-	if (_is_ahci_cntrl(path))
-		type = CNTRL_TYPE_AHCI;
+	} else if (_is_storage_controller(path)) {
+		if (_is_ahci_cntrl(path))
+			type = CNTRL_TYPE_AHCI;
+		else if (_is_isci_cntrl(path)
+				|| sysfs_enclosure_attached_to_cntrl(path)
+				|| _is_smp_cntrl(path))
+			type = CNTRL_TYPE_SCSI;
+	}
 	return type;
 }
 
@@ -139,6 +157,7 @@ struct _host_type *alloc_host(int id, struct _host_type *next)
 		host->ibpi_state_buffer = NULL;
 		memset(host->bitstream, 0, sizeof(host->bitstream));
 		host->flush = 0;
+		host->ports = 0;
 		host->next = next;
 	}
 	return host;
@@ -161,6 +180,8 @@ void _find_host(const char *path, struct _host_type **hosts)
 	char *p;
 	int index = -1;
 	struct _host_type *th;
+	DIR *d;
+	struct dirent *de;
 
 	p = strrchr(path, '/');
 	if (!p++)
@@ -170,6 +191,21 @@ void _find_host(const char *path, struct _host_type **hosts)
 		th = alloc_host(index, (*hosts) ? (*hosts) : NULL);
 		if (!th)
 			return;
+
+		d = opendir(path);
+		if(!d) {
+			free(th);
+			return;
+		}
+
+		while ((de = readdir(d))) {
+			if (strncmp(de->d_name, "phy-", strlen("phy-")) == 0) {
+				th->ports++;
+			}
+		}
+
+		closedir(d);
+
 		*hosts = th;
 	}
 }
@@ -260,18 +296,17 @@ static unsigned int _ahci_em_messages(const char *path)
 struct cntrl_device *cntrl_device_init(const char *path)
 {
 	unsigned int em_enabled;
-	enum int_cntrl_type type;
+	enum cntrl_type type;
 	struct cntrl_device *device = NULL;
 
 	type = _get_type(path);
-	if (type != INT_CNTRL_TYPE_UNKNOWN) {
+	if (type != CNTRL_TYPE_UNKNOWN) {
 		switch (type) {
-		case INT_CNTRL_TYPE_DELLSSD:
-		case INT_CNTRL_TYPE_ISCI:
-		case INT_CNTRL_TYPE_SCSI:
+		case CNTRL_TYPE_DELLSSD:
+		case CNTRL_TYPE_SCSI:
 			em_enabled = 1;
 			break;
-		case INT_CNTRL_TYPE_AHCI:
+		case CNTRL_TYPE_AHCI:
 			em_enabled = _ahci_em_messages(path);
 			break;
 		default:
@@ -280,15 +315,14 @@ struct cntrl_device *cntrl_device_init(const char *path)
 		if (em_enabled) {
 			device = malloc(sizeof(struct cntrl_device));
 			if (device) {
-				if (type == INT_CNTRL_TYPE_ISCI) {
-					device->isci_present = 1;
+				if (type == CNTRL_TYPE_SCSI) {
+					device->isci_present = _is_isci_cntrl(path);
 					device->hosts = _cntrl_get_hosts(path);
-					type = CNTRL_TYPE_SCSI;
 				} else {
 					device->isci_present = 0;
 					device->hosts = NULL;
 				}
-				device->cntrl_type = (enum cntrl_type)type;
+				device->cntrl_type = type;
 				device->sysfs_path = strdup(path);
 			}
 		} else {

src/cntrl.h
@@ -20,6 +20,8 @@
 #ifndef _CNTRL_H_INCLUDED_
 #define _CNTRL_H_INCLUDED_
 
+#include "smp.h"
+
 /**
  * This enumeration type lists all supported storage controller types.
  */
@@ -55,7 +57,7 @@ struct cntrl_device {
 		/**
 		 * ibpi state buffer for directly attached devices
 		 */
-		void *ibpi_state_buffer;
+		struct gpio_tx_register_byte *ibpi_state_buffer;
 		/**
 		 * outbound raw byte stream
 		 */
@@ -68,6 +70,10 @@ struct cntrl_device {
 		 * host identifier for different hba instances
 		 */
 		int host_id;
+		/**
+		 * number of total phy ports
+		 */
+		int ports;
 		/**
 		 * pointer to next structure
 		 */

src/ledmon.c
@@ -679,15 +679,15 @@ static void _revalidate_dev(struct block_device *block)
 			  block->sysfs_path, block->cntrl_path);
 		return;
 	}
-	if (block->cntrl->cntrl_type == CNTRL_TYPE_SCSI &&
-	    block->cntrl->isci_present) {
+	if (block->cntrl->cntrl_type == CNTRL_TYPE_SCSI) {
 		block->host = block_get_host(block->cntrl, block->host_id);
 		if (block->host) {
-			isci_cntrl_init_smp(NULL, block->cntrl);
+			if (dev_directly_attached(block->sysfs_path))
+				cntrl_init_smp(NULL, block->cntrl);
 		} else {
 			log_debug("Failed to get host for dev: %s, hostId: %d",
 				  block->sysfs_path, block->host_id);
-			/* If hosts failed for isci, invalidate cntrl */
+			/* If failed, invalidate cntrl */
 			block->cntrl = NULL;
 		}
 	}

src/smp.h
@@ -17,8 +17,51 @@
  *
  */
 
+#include "ibpi.h"
+#include "block.h"
+
 #ifndef _SMP_H_INCLUDED
 #define _SMP_H_INCLUDED
+
+/* smp constants */
+#define SMP_FRAME_TYPE_REQ	0x40
+#define SMP_FRAME_TYPE_RESP	0x41
+
+#define SMP_FUNC_GPIO_READ	0x02
+#define SMP_FUNC_GPIO_WRITE	0x82
+
+#define SMP_FRAME_CRC_LEN		sizeof(uint32_t)
+#define SMP_DATA_CHUNK_SIZE	sizeof(uint32_t)
+
+/* gpio constants */
+/* gpio register types */
+#define GPIO_REG_TYPE_CFG		0x00
+#define GPIO_REG_TYPE_RX		0x01
+#define GPIO_REG_TYPE_RX_GP	0x02
+#define GPIO_REG_TYPE_TX		0x03
+#define GPIO_REG_TYPE_TX_GP	0x04
+
+/* gpio register indexes */
+#define GPIO_REG_IND_CFG_0	0x00
+#define GPIO_REG_IND_CFG_1	0x01
+
+#define GPIO_REG_IND_RX_0		0x00
+#define GPIO_REG_IND_RX_1		0x01
+
+#define GPIO_REG_IND_TX_0		0x00
+#define GPIO_REG_IND_TX_1		0x01
+
+#define SG_RESPONSE_TIMEOUT (5 * 1000)	/* 1000 as miliseconds multiplier */
+#define SCSI_MAX_CDB_LENGTH	0x10
+
+#define GPIO_STATUS_OK			0x00
+#define GPIO_STATUS_FAILURE 0x80
+
+struct gpio_tx_register_byte {
+	unsigned char error:3;
+	unsigned char locate:2;
+	unsigned char activity:3;
+} __attribute__ ((__packed__));
 
 /**
  * @brief Sends message to SES processor of an enclosure.
@@ -70,6 +113,27 @@ int scsi_smp_write_buffer(struct block_device *device);
  * @return Phy index on success if path and cntrl weren't NULL
  *         0 if error occurred or path was NULL.
  */
-int isci_cntrl_init_smp(const char *path, struct cntrl_device *cntrl);
+int cntrl_init_smp(const char *path, struct cntrl_device *cntrl);
+
+/**
+ * @brief Write GPIO data
+ *
+ * @param[in]      path            Path to the device in sysfs.
+ *                                 phy.
+ *
+ * @param[in]      smp_reg_type    GPIO register type
+ *
+ * @param[in]      smp_reg_index   GPIO register index
+ *
+ * @param[in]      smp_reg_count   GPIO register count
+ *
+ * @param[in]      data            Data to be written
+ *
+ * @return written register count
+ *         <0 if error occurred
+ */
+int smp_write_gpio(const char *path, int smp_reg_type,
+			   int smp_reg_index, int smp_reg_count, void *data,
+			   size_t len);
 
 #endif				/* _SCSI_H_INCLUDED_ */

commit 5ebe4bb3170da47f73daf33e7ac321792d308422

Fix wrong drive ordering in SMP GPIO_TX registers
Fixes: 1de67a0cd449 ("add support for SGPIO via SMP GPIO_REG_TYPE_TX register")
Signed-off-by: Artur Paszkiewicz <artur.paszkiewicz@intel.com>

src/smp.c
@@ -494,7 +494,12 @@ int scsi_smp_fill_buffer(struct block_device *device, enum ibpi_pattern ibpi)
 		set_raw_pattern(device->phy_index,
 			&device->host->bitstream[0], &ibpi2sgpio[ibpi].pattern);
 	} else {
-		device->host->ibpi_state_buffer[device->phy_index] =
+		/*
+		 * GPIO_TX[n] register has the highest numbered drive of the
+		 * four in the first byte and the lowest numbered drive in the
+		 * fourth byte. See SFF-8485 Rev. 0.7 Table 24.
+		 */
+		gpio_tx[device->phy_index + 3 - (device->phy_index % 4) * 2] =
 			ibpi2sgpio[ibpi].pattern;
 	}
 
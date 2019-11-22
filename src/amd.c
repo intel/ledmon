ceba1fec3000d9dbea608ba1ebc24b19866b79e5
Nathan Fontenot <nathan.fontenot@amd.com>
2019-11-22 13:23:04 -0600

    Add platform check for AMD systems
    To support IPMI and SGPIO interfaces on different AMD platforms, we need
    to determine what platform we are running on. To do this we check dmi
    data to control behavior.
    
    Signed-off-by: Nathan Fontenot <nathan.fontenot@amd.com>

--- src/amd.c
+++ src/amd.c
@@ -43,6 +43,9 @@
 #include "amd.h"
 #include "amd_sgpio.h"
 
+enum amd_led_interfaces amd_interface = AMD_INTF_UNSET;
+enum amd_platforms amd_platform = AMD_PLATFORM_UNSET;
+
 int _find_file_path(const char *start_path, const char *filename,
 		    char *path, size_t path_len)
 {
@@ -92,21 +95,98 @@ int _find_file_path(const char *start_path, const char *filename,
 	return found;
 }
 
+static void _get_amd_led_interface(void)
+{
+	char *name;
+
+	name = get_text("/sys/class/dmi/id", "product_name");
+	if (!name)
+		return;
+
+	if (!strncmp(name, "ETHANOL_X", 9)) {
+		amd_interface = AMD_INTF_IPMI;
+		amd_platform = AMD_PLATFORM_ETHANOL_X;
+	} else if (!strncmp(name, "DAYTONA_X", 9)) {
+		amd_interface = AMD_INTF_IPMI;
+		amd_platform = AMD_PLATFORM_DAYTONA_X;
+	} else if (!strncmp(name, "GRANDSTAND", 10)) {
+		amd_interface = AMD_INTF_SGPIO;
+		amd_platform = AMD_PLATFORM_GRANDSTAND;
+	} else if (!strncmp(name, "Speedway", 8)) {
+		amd_interface = AMD_INTF_SGPIO;
+		amd_platform = AMD_PLATFORM_SPEEDWAY;
+	}
+
+	free(name);
+}
+
 int amd_em_enabled(const char *path)
 {
-	return _amd_sgpio_em_enabled(path);
+	int rc;
+
+	_get_amd_led_interface();
+
+	switch (amd_interface) {
+	case AMD_INTF_SGPIO:
+		rc = _amd_sgpio_em_enabled(path);
+		break;
+	case AMD_INTF_IPMI:
+		log_error("IPMI not supported\n");
+		rc = -EOPNOTSUPP;
+		break;
+	default:
+		log_error("Unsupported AMD interface\n");
+		rc = -EOPNOTSUPP;
+		break;
+	}
+
+	return rc;
 }
 
 int amd_write(struct block_device *device, enum ibpi_pattern ibpi)
 {
+	int rc;
+
 	/* write only if state has changed */
 	if (ibpi == device->ibpi_prev)
 		return 1;
 
-	return _amd_sgpio_write(device, ibpi);
+	switch (amd_interface) {
+	case AMD_INTF_SGPIO:
+		rc = _amd_sgpio_write(device, ibpi);
+		break;
+	case AMD_INTF_IPMI:
+		log_error("IPMI not supported\n");
+		rc = -EOPNOTSUPP;
+		break;
+	case AMD_INTF_UNSET:
+	default:
+		log_error("Unsupported AMD interface\n");
+		rc = -EOPNOTSUPP;
+		break;
+	}
+
+	return rc;
 }
 
 char *amd_get_path(const char *cntrl_path)
 {
-	return _amd_sgpio_get_path(cntrl_path);
+	char *path;
+
+	switch (amd_interface) {
+	case AMD_INTF_SGPIO:
+		path = _amd_sgpio_get_path(cntrl_path);
+		break;
+	case AMD_INTF_IPMI:
+		log_error("IPMI not supported\n");
+		path = NULL;
+		break;
+	case AMD_INTF_UNSET:
+	default:
+		log_error("Unsupported AMD interface\n");
+		path = NULL;
+		break;
+	}
+
+	return path;
 }

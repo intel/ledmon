intel/ledmon

commit 2a66a3c37bbf6e52e1b4119459ebf7dfd86ae854

Add platform check for AMD systems
To support IPMI and SGPIO interfaces on different AMD platforms, we need
to determine what platform we are running on. To do this we check dmi
data to control behavior.

Signed-off-by: Nathan Fontenot <nathan.fontenot@amd.com>

src/amd.c
@@ -43,6 +43,9 @@
 #include "amd.h"
 #include "amd_sgpio.h"
 
+enum amd_led_interfaces amd_interface = AMD_INTF_UNSET;
+enum amd_platforms amd_platform = AMD_PLATFORM_UNSET;
+
 int _find_file_path(const char *start_path, const char *filename,
 		    char *path, size_t path_len)
 {
@@ -92,21 +95,98 @@ int _find_file_path(const char *start_path, const char *filename,
 	return found;
 }
 
+static void _get_amd_led_interface(void)
+{
+	char *name;
+
+	name = get_text("/sys/class/dmi/id", "product_name");
+	if (!name)
+		return;
+
+	if (!strncmp(name, "ETHANOL_X", 9)) {
+		amd_interface = AMD_INTF_IPMI;
+		amd_platform = AMD_PLATFORM_ETHANOL_X;
+	} else if (!strncmp(name, "DAYTONA_X", 9)) {
+		amd_interface = AMD_INTF_IPMI;
+		amd_platform = AMD_PLATFORM_DAYTONA_X;
+	} else if (!strncmp(name, "GRANDSTAND", 10)) {
+		amd_interface = AMD_INTF_SGPIO;
+		amd_platform = AMD_PLATFORM_GRANDSTAND;
+	} else if (!strncmp(name, "Speedway", 8)) {
+		amd_interface = AMD_INTF_SGPIO;
+		amd_platform = AMD_PLATFORM_SPEEDWAY;
+	}
+
+	free(name);
+}
+
 int amd_em_enabled(const char *path)
 {
-	return _amd_sgpio_em_enabled(path);
+	int rc;
+
+	_get_amd_led_interface();
+
+	switch (amd_interface) {
+	case AMD_INTF_SGPIO:
+		rc = _amd_sgpio_em_enabled(path);
+		break;
+	case AMD_INTF_IPMI:
+		log_error("IPMI not supported\n");
+		rc = -EOPNOTSUPP;
+		break;
+	default:
+		log_error("Unsupported AMD interface\n");
+		rc = -EOPNOTSUPP;
+		break;
+	}
+
+	return rc;
 }
 
 int amd_write(struct block_device *device, enum ibpi_pattern ibpi)
 {
+	int rc;
+
 	/* write only if state has changed */
 	if (ibpi == device->ibpi_prev)
 		return 1;
 
-	return _amd_sgpio_write(device, ibpi);
+	switch (amd_interface) {
+	case AMD_INTF_SGPIO:
+		rc = _amd_sgpio_write(device, ibpi);
+		break;
+	case AMD_INTF_IPMI:
+		log_error("IPMI not supported\n");
+		rc = -EOPNOTSUPP;
+		break;
+	case AMD_INTF_UNSET:
+	default:
+		log_error("Unsupported AMD interface\n");
+		rc = -EOPNOTSUPP;
+		break;
+	}
+
+	return rc;
 }
 
 char *amd_get_path(const char *cntrl_path)
 {
-	return _amd_sgpio_get_path(cntrl_path);
+	char *path;
+
+	switch (amd_interface) {
+	case AMD_INTF_SGPIO:
+		path = _amd_sgpio_get_path(cntrl_path);
+		break;
+	case AMD_INTF_IPMI:
+		log_error("IPMI not supported\n");
+		path = NULL;
+		break;
+	case AMD_INTF_UNSET:
+	default:
+		log_error("Unsupported AMD interface\n");
+		path = NULL;
+		break;
+	}
+
+	return path;
 }

src/amd.h
@@ -26,6 +26,24 @@ struct amd_drive {
 	int		initiator;
 };
 
+enum amd_led_interfaces {
+	AMD_INTF_UNSET,
+	AMD_INTF_SGPIO,
+	AMD_INTF_IPMI,
+};
+
+extern enum amd_led_interfaces amd_interface;
+
+enum amd_platforms {
+	AMD_PLATFORM_UNSET,
+	AMD_PLATFORM_ETHANOL_X,
+	AMD_PLATFORM_DAYTONA_X,
+	AMD_PLATFORM_GRANDSTAND,
+	AMD_PLATFORM_SPEEDWAY,
+};
+
+extern enum amd_platforms amd_platform;
+
 int amd_em_enabled(const char *path);
 int amd_write(struct block_device *device, enum ibpi_pattern ibpi);
 char *amd_get_path(const char *cntrl_path);

commit 7882127846209438e09a03e3119f9bfa74f57953

Default to SGPIO for AMD systems
A recent update to add support for IPMI on AMD systems changed
the behavior when checking for enclosure management enablement.
Previously any AMD system that returned success from
_amd_sgpio_em_enabled() would use SGPIO. The update added an explicit
check for DMI system ids that broke this behavior.

This patch corrects that behavior by defaulting to SGPIO for
AMD systems and only using the DMI id to check for systems
known to support IPMI.

As part of this change enum amd_platform was changed to enum
amd_ipmi_platforms as we only need to track systems for IPMI.
This is needed so we can determine the correct channel and slave
address for IPMI commands.

Fixes: 2a66a3c37bbf ("Add platform check for AMD systems")

Signed-off-by: Nathan Fontenot <nathan.fontenot@amd.com>

src/amd.c
@@ -45,7 +45,7 @@
 #include "amd_ipmi.h"
 
 enum amd_led_interfaces amd_interface = AMD_INTF_UNSET;
-enum amd_platforms amd_platform = AMD_PLATFORM_UNSET;
+enum amd_ipmi_platforms amd_ipmi_platform = AMD_PLATFORM_UNSET;
 
 int _find_file_path(const char *start_path, const char *filename,
 		    char *path, size_t path_len)
@@ -96,36 +96,29 @@ int _find_file_path(const char *start_path, const char *filename,
 	return found;
 }
 
-static void _get_amd_led_interface(void)
-{
-	char *name;
-
-	name = get_text("/sys/class/dmi/id", "product_name");
-	if (!name)
-		return;
-
-	if (!strncmp(name, "ETHANOL_X", 9)) {
-		amd_interface = AMD_INTF_IPMI;
-		amd_platform = AMD_PLATFORM_ETHANOL_X;
-	} else if (!strncmp(name, "DAYTONA_X", 9)) {
-		amd_interface = AMD_INTF_IPMI;
-		amd_platform = AMD_PLATFORM_DAYTONA_X;
-	} else if (!strncmp(name, "GRANDSTAND", 10)) {
-		amd_interface = AMD_INTF_SGPIO;
-		amd_platform = AMD_PLATFORM_GRANDSTAND;
-	} else if (!strncmp(name, "Speedway", 8)) {
-		amd_interface = AMD_INTF_SGPIO;
-		amd_platform = AMD_PLATFORM_SPEEDWAY;
-	}
-
-	free(name);
-}
-
+/* For AMD platforms to use IPMI for LED control we need to know
+ * the platform we're running on. This enables us to select the
+ * proper channel and slave address when making IPMI requests.
+ * Platforms not checked for IPMI enablement default to using SGPIO.
+ */
 int amd_em_enabled(const char *path)
 {
+	char *platform;
 	int rc;
 
-	_get_amd_led_interface();
+	/* Default to SGPIO interface */
+	amd_interface = AMD_INTF_SGPIO;
+
+	platform = get_text("/sys/class/dmi/id", "product_name");
+
+	/* Check IPMI platforms */
+	if (!strncmp(platform, "ETHANOL_X", 9)) {
+		amd_interface = AMD_INTF_IPMI;
+		amd_ipmi_platform = AMD_PLATFORM_ETHANOL_X;
+	} else if (!strncmp(platform, "DAYTONA_X", 9)) {
+		amd_interface = AMD_INTF_IPMI;
+		amd_ipmi_platform = AMD_PLATFORM_DAYTONA_X;
+	}
 
 	switch (amd_interface) {
 	case AMD_INTF_SGPIO:
@@ -135,7 +128,8 @@ int amd_em_enabled(const char *path)
 		rc = _amd_ipmi_em_enabled(path);
 		break;
 	default:
-		log_error("Unsupported AMD interface\n");
+		log_error("Unknown interface for AMD %s platform\n",
+			  platform);
 		rc = -EOPNOTSUPP;
 		break;
 	}

src/amd.h
@@ -39,15 +39,13 @@ enum amd_led_interfaces {
 
 extern enum amd_led_interfaces amd_interface;
 
-enum amd_platforms {
+enum amd_ipmi_platforms {
 	AMD_PLATFORM_UNSET,
 	AMD_PLATFORM_ETHANOL_X,
 	AMD_PLATFORM_DAYTONA_X,
-	AMD_PLATFORM_GRANDSTAND,
-	AMD_PLATFORM_SPEEDWAY,
 };
 
-extern enum amd_platforms amd_platform;
+extern enum amd_ipmi_platforms amd_ipmi_platform;
 
 int amd_em_enabled(const char *path);
 int amd_write(struct block_device *device, enum ibpi_pattern ibpi);

src/amd_ipmi.c
@@ -105,7 +105,7 @@ static int _get_ipmi_nvme_port(char *path)
 	/* Some platfroms require an adjustment to the port value based
 	 * on how they are numbered by the BIOS.
 	 */
-	switch (amd_platform) {
+	switch (amd_ipmi_platform) {
 	case AMD_PLATFORM_DAYTONA_X:
 		port -= 2;
 		break;
@@ -202,7 +202,7 @@ static int _ipmi_platform_channel(struct amd_drive *drive)
 {
 	int rc = 0;
 
-	switch (amd_platform) {
+	switch (amd_ipmi_platform) {
 	case AMD_PLATFORM_ETHANOL_X:
 		drive->channel =  0xd;
 		break;
@@ -222,7 +222,7 @@ static int _ipmi_platform_slave_address(struct amd_drive *drive)
 {
 	int rc = 0;
 
-	switch (amd_platform) {
+	switch (amd_ipmi_platform) {
 	case AMD_PLATFORM_ETHANOL_X:
 		drive->slave_addr = 0xc0;
 		break;
@@ -375,12 +375,12 @@ int _amd_ipmi_em_enabled(const char *path)
 		     &data_sz, &status);
 
 	if (rc) {
-		log_error("Can't determine MG9098 Status\n");
+		log_error("Can't determine MG9098 Status for AMD platform\n");
 		return 0;
 	}
 
 	if (status != 98) {
-		log_error("Not a MG9098\n");
+		log_error("Platform %s does not have a MG9098 controller\n");
 		return 0;
 	}
 
intel/ledmon

commit ff49cce8ec37078de8ce957f8022eae8bde6651b

Allow to run ledctl version without root
Change-Id: I78cb907ccbcdc5650f9eb73992320b391d915224
Signed-off-by: Kinga Tanska <kinga.tanska@intel.com>

src/ledctl.c
@@ -521,6 +521,41 @@ static status_t _cmdline_ibpi_parse(int argc, char *argv[])
 }
 
 /**
+ * @brief Command line parser - checks if command line input contains
+ * options which don't require to run ledctl as root.
+ *
+ * The function parses options of ledctl application.
+ * It handles option to print version and help.
+ *
+ * @param[in]      argc           number of elements in argv array.
+ * @param[in]      argv           command line arguments.
+ *
+ * @return STATUS_SUCCESS if successful, otherwise a valid status_t status code.
+ */
+static status_t _cmdline_parse_non_root(int argc, char *argv[])
+{
+	int opt_index, opt = -1;
+	status_t status = STATUS_SUCCESS;
+
+	do {
+		opt = getopt_long(argc, argv, shortopt, longopt, &opt_index);
+		switch (opt) {
+		case 'v':
+			_ledctl_version();
+			exit(EXIT_SUCCESS);
+		case 'h':
+			_ledctl_help();
+			exit(EXIT_SUCCESS);
+		case ':':
+		case '?':
+			return STATUS_CMDLINE_ERROR;
+		}
+	} while (opt >= 0);
+
+	return status;
+}
+
+/**
  * @brief Command line parser - options.
  *
  * This is internal function of ledctl utility. The function parses options of
@@ -559,12 +594,6 @@ static status_t _cmdline_parse(int argc, char *argv[])
 
 			}
 			break;
-		case 'v':
-			_ledctl_version();
-			exit(EXIT_SUCCESS);
-		case 'h':
-			_ledctl_help();
-			exit(EXIT_SUCCESS);
 		case 'l':
 			status = set_log_path(optarg);
 			break;
@@ -679,6 +708,10 @@ int main(int argc, char *argv[])
 	setup_options(&longopt, &shortopt, possible_params,
 			possible_params_size);
 	set_invocation_name(argv[0]);
+
+	if (_cmdline_parse_non_root(argc, argv) != STATUS_SUCCESS)
+		return STATUS_CMDLINE_ERROR;
+
 	openlog(progname, LOG_PERROR, LOG_USER);
 
 	if (getuid() != 0) {

commit ef67df720a8f464c76e545c45d0841f57a7f9090

ledctl: Correct option parsing with non-root users
A recent update to allow ledctl to run as non-root user does not
correctly handle calling getopt_long() twice. Before making the
second call to getopt_long() the getopt variable 'optind' needs
to be reset. Without doing this the second call to getopt_long()
fails because it believes all of the options have been parsed.

This patch resets the optind variable before the second getopt_long()
call.

Fixes: ff49cce8ec37 ("Allow to run ledctl version without root")
Signed-off-by: Nathan Fontenot <nathan.fontenot@amd.com>

src/ledctl.c
@@ -572,6 +572,8 @@ static status_t _cmdline_parse(int argc, char *argv[])
 	int opt, opt_index = -1;
 	status_t status = STATUS_SUCCESS;
 
+	optind = 1;
+
 	do {
 		opt = getopt_long(argc, argv, shortopt, longopt, &opt_index);
 		if (opt == -1)
775567b0f63b8fe0a3a51f84c50520c2b76968db
Tony Asleson <tasleson@redhat.com>
2023-08-21 10:23:12 -0500

    runtests.py: Correct for ubuntu
    pytest is pytest-3 on ubuntu.
    
    Signed-off-by: Tony Asleson <tasleson@redhat.com>

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -2,6 +2,13 @@
 #
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+PYTEST=pytest
+
+if ! command -v $PYTEST &> /dev/null
+then
+	PYTEST=pytest-3
+fi
+
 #Put us in a consistent spot
 if [ ! -e "./tests" ];then
     echo "We are running from test folder directly."
@@ -9,7 +16,7 @@ if [ ! -e "./tests" ];then
 fi
 
 echo "exercising ledctl"
-pytest tests --ledctl-binary=src/ledctl/ledctl --slot-filters="$LEDMONTEST_SLOT_FILTER" || exit 1
+$PYTEST tests --ledctl-binary=src/ledctl/ledctl --slot-filters="$LEDMONTEST_SLOT_FILTER" || exit 1
 
 echo "running library unit test"
 ./tests/lib_unit_test || exit 1

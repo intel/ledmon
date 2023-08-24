36a7695484afa66d6e30d1c908efc48f9cfd2fa2
Tony Asleson <tasleson@redhat.com>
2023-08-24 12:00:29 -0500

    Add script to check public symbols
    We want to check that all the public symbols in the shared library
    are prefixed with "led_" to ensure we don't have symbols public which
    shouldn't be.
    
    Signed-off-by: Tony Asleson <tasleson@redhat.com>

---
+++ tests/check_symbol_visibility.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (C) 2023 Red Hat Inc.
+#
+# Ensure we only have shared library symbols which start with "led_*"
+#
+
+tmpf=$(mktemp /tmp/ledmon_sym.XXXXXXXX) || exit 1
+
+function cleanup {
+        rm  -f "${tmpf}" || exit 1
+}
+
+trap cleanup EXIT
+
+nm -D src/lib/.libs/libled.so | grep " T " | grep -v " led_" > "${tmpf}"
+
+found=$(wc -l < "${tmpf}") || exit 1
+
+if [[ ${found} -ne "0" ]]; then
+        echo "ERROR: Public symbols found which don't have led_ prefix for shared library"
+        cat "${tmpf}"
+        exit 1
+fi
+
+exit 0

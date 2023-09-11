d805c6a310f3edd07e860feef440f5c20d87ebcc
Pawel Piatkowski <pawel.piatkowski@intel.com>
2023-09-11 15:21:12 +0200

    Add github action to run pytest
    Add running python tests as github action
    
    Signed-off-by: Pawel Piatkowski <pawel.piatkowski@intel.com>

---
+++ tests/install_ubuntu_packages.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+VERSION_CODENAME=$(grep -oP '(?<=^VERSION_CODENAME=).+' /etc/os-release | tr -d '"')
+echo "Detected VERSION_CODENAME: $VERSION_CODENAME"
+
+# Add ubuntu repository
+sudo add-apt-repository "deb [arch=amd64] http://archive.ubuntu.com/ubuntu $VERSION_CODENAME \
+        main universe"
+# Install gcc
+sudo apt-get update && sudo apt-get install gcc-$1 g++-$1
+# Install dependencies
+sudo apt-get install pkg-config automake autoconf autoconf-archive make libsgutils2-dev \
+        libudev-dev libpci-dev check

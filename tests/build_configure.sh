6fbe6237b1eb5e61fe417ab20a1e9abd7736de40
Kinga Tanska <kinga.tanska@intel.com>
2023-10-20 09:22:33 +0200

    fixes after review
    Rename script. Do not execute if CFLAGS is not set.
    
    Signed-off-by: Kinga Tanska <kinga.tanska@intel.com>

--- tests/build_configure.sh
+++ tests/build_configure.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/bash
 
 # Set compiler flags
-export CFLAGS=$1
+if ! [[ -z $1 ]];
+then
+	export CFLAGS=$1
+else
+	echo "There is no CFLAGS set. Please check your configuration."
+	exit 1
+fi
+
 if [ -z $2 ]
 then
 	set CC=$2

cf0f37bdd3e03f7fb0e9e9a2163b2a2f49fa1248
Mariusz Tkaczyk <mariusz.tkaczyk@linux.intel.com>
2023-11-20 15:35:02 +0100

    tests, configure: small adjustments
    As a default use ledctl from repository for testing. Fix typo in
    conftest. Print ENABLE_TEST flag.
    
    Signed-off-by: Mariusz Tkaczyk <mariusz.tkaczyk@linux.intel.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import logging
 LOGGER = logging.getLogger(__name__)
 
 def pytest_addoption(parser):
-    parser.addoption("--ledctl-binary", action="store", default="/usr/sbin/ledctl")
+    parser.addoption("--ledctl-binary", action="store", default="src/ledctl/ledctl")
     parser.addoption("--slot-filters", action="store", default="none")
 
 def pytest_collection_modifyitems(session, config, items):
@@ -11,9 +11,9 @@ def pytest_collection_modifyitems(session, config, items):
     LOGGER.info(f'[CONFIG] slot filters: {config.getoption("--slot-filters")}')
 
 def pytest_generate_tests(metafunc):
-        ledtctl_option_value = metafunc.config.option.ledctl_binary
-        if 'ledctl_binary' in metafunc.fixturenames and ledtctl_option_value is not None:
-                metafunc.parametrize("ledctl_binary", [ledtctl_option_value])
+        ledctl_option_value = metafunc.config.option.ledctl_binary
+        if 'ledctl_binary' in metafunc.fixturenames and ledctl_option_value is not None:
+                metafunc.parametrize("ledctl_binary", [ledctl_option_value])
 
         slotfilters_option_value = metafunc.config.option.slot_filters
         if 'slot_filters' in metafunc.fixturenames and slotfilters_option_value is not None:

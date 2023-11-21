59e1edda60332c5d56f742f57aaadd6d926d2af0
Mariusz Tkaczyk <mariusz.tkaczyk@linux.intel.com>
2023-11-21 13:48:09 +0100

    tests: introduce ledctl_cmd class
    Create class to avoid creating similar API in different tests.
    Use it for slot_test.py. Refactor and simplify few functions.
    
    Signed-off-by: Mariusz Tkaczyk <mariusz.tkaczyk@linux.intel.com>

--- tests/ledctl/slot_test.py
+++ tests/ledctl/slot_test.py
@@ -4,10 +4,8 @@
 # Simple tests for ledctl.  This needs to be expanded.
 
 import pytest
-import sys
-import subprocess
-import re
 import logging
+from ledctl_cmd import LedctlCmd
 
 LOGGER = logging.getLogger(__name__)
 
@@ -18,137 +16,37 @@ LOGGER = logging.getLogger(__name__)
 # To run this test do `pytest tests --ledctl-binary=../src/ledctl --slot-filters=sg3-,sg2-`
 # to filter out enclosures that don't work
 
+def test_non_slot_set_path(ledctl_binary, slot_filters):
+    """
+    Test setting the led status by using non-slot syntax, eg. ledctl locate=/dev/sda
+    """
 
-class Slot:
-    def __init__(self, cntrl_type, slot_id, state, device_node):
-        self.cntrl_type = cntrl_type
-        self.slot = slot_id
-        self.state = state.lower()
-        self.device_node = None
-        if device_node.startswith('/dev/'):
-            self.device_node = device_node
+    cmd = LedctlCmd(ledctl_binary, slot_filters)
+    slots_with_device_nodes = [s for s in cmd.get_all_slots() if s.device_node is not None]
 
-    def __str__(self):
-        return "slot: %s state: %s device: %s" % (self.slot, self.state, self.device_node)
+    if len(slots_with_device_nodes) == 0:
+        pytest.skip("This test requires slots with devices but none found.")
 
+    for slot in slots_with_device_nodes:
+        for state in ["failure", "locate", "normal"]:
+            cmd.set_ibpi(slot.device_node, state)
+            cur = cmd.get_slot(slot)
+            assert cur.state == state,\
+                f"unable to set from \"{slot}\" to \"{state}\", current = \"{cur}\" using non-slot syntax"
 
-class TestSlot():
+def test_slot_state_walk(ledctl_binary, slot_filters):
+    """
+    Test that we can set slots to different states and verify that they reported a change
+    """
 
-    ledctl_bin = ""
+    cmd = LedctlCmd(ledctl_binary, slot_filters)
+    slots = cmd.get_all_slots()
 
-    def get_controllers_with_slot_functionality(self):
-        supported = ["SCSI", "VMD", "NPEM"]
-        rc = {}
-        result = subprocess.run(["sudo", self.ledctl_bin, "--list-controllers"],
-                                capture_output=True, check=True)
-        out = result.stdout.decode("utf-8")
-        for raw_line in out.split("\n"):
-            line = raw_line.strip()
-            for ctrl in supported:
-                if ctrl in line:
-                    rc[ctrl] = True
-                    break
-        return rc.keys()
+    if len(slots) == 0:
+        pytest.skip("This test requires any slot but none found.")
 
-    @staticmethod
-    def process_slot_line(controller, rawline, slot_filters):
-        regex_pat = r"^slot: (.+)led state:(.+)device:(.*)$"
-        slot_line_re = re.compile(regex_pat)
-        line = rawline.strip()
-        if len(line) == 0:
-            return None
-        match = slot_line_re.match(line)
-        if match is None:
-            raise Exception(f"Text line '{line}' did not match regex '{regex_pat}'")
-
-        slot = Slot(controller, match.group(1).strip(), match.group(2).strip(), match.group(3).strip())
-
-        if slot_filters == "none":
-            return slot
-
-        for slot_filter in slot_filters:
-            if slot.slot.startswith(slot_filter):
-                # Filter out this slot
-                return None
-        return slot
-
-    def get_slot(self, slot_o, slot_filter):
-        result = subprocess.run(["sudo",
-                                self.ledctl_bin,
-                                "--get-slot",
-                                "--controller-type", slot_o.cntrl_type,
-                                "--slot", slot_o.slot], capture_output=True, check=True)
-        out = result.stdout.decode("utf-8")
-        return TestSlot.process_slot_line(slot_o.cntrl_type, out, slot_filter)
-
-    def get_slots(self, controller_type, slot_filter):
-        rc = []
-        result = subprocess.run(["sudo",
-                                 self.ledctl_bin,
-                                "--list-slots",
-                                "--controller-type", controller_type], capture_output=True)
-        if result.returncode == 0:
-            out = result.stdout.decode("utf-8")
-            for line in out.split("\n"):
-                s = TestSlot.process_slot_line(controller_type, line, slot_filter)
-                if s is not None:
-                    rc.append(s)
-        return rc
-
-    def set_slot_state(self, slot_o, state):
-        subprocess.run(["sudo",
-                        self.ledctl_bin,
-                        "--set-slot",
-                        "--controller-type", slot_o.cntrl_type,
-                        "--slot", slot_o.slot,
-                        "--state", state], capture_output=True, check=True)
-
-    def get_all_slots(self, slot_filter):
-        all_slots = []
-        for controller in self.get_controllers_with_slot_functionality():
-            all_slots.extend(self.get_slots(controller, slot_filter))
-        return all_slots
-
-    def set_state_by_dev_node(self, dev_node, state):
-        option = "%s=%s" % (state, dev_node)
-        result = subprocess.run(["sudo", self.ledctl_bin, option], capture_output=True)
-        if result.returncode == 0:
-            return True
-        else:
-            LOGGER.error(f"Error while using led non slot syntax {option} {result}")
-            return False
-
-    def test_non_slot_set_path(self, ledctl_binary, slot_filters):
-        """
-        Test setting the led status by using non-slot syntax, eg. ledctl locate=/dev/sda
-        :return:
-        """
-        self.ledctl_bin = ledctl_binary
-        slots_with_device_nodes = [s for s in self.get_all_slots(slot_filters) if s.device_node is not None]
-
-        if len(slots_with_device_nodes) == 0:
-            pytest.skip("This test requires slots with devices but none found.")
-
-        for slot in slots_with_device_nodes:
-            for state in ["failure", "locate", "normal"]:
-                self.set_state_by_dev_node(slot.device_node, state)
-                cur = self.get_slot(slot, slot_filters)
-                assert cur.state == state,\
-                    f"unable to set from {slot} to {state}, current = {cur} using non-slot syntax"
-
-    def test_slot_state_walk(self, ledctl_binary, slot_filters):
-        """
-        Test that we can set slots to different states and verify that they reported a change
-        :return:
-        """
-        self.ledctl_bin = ledctl_binary
-        slots = self.get_all_slots(slot_filters)
-
-        if len(slots) == 0:
-            pytest.skip("This test requires any slot but none found.")
-
-        for slot in slots:
-            for state in ["locate", "failure", "normal"]:
-                self.set_slot_state(slot, state)
-                cur = self.get_slot(slot, slot_filters)
-                assert cur.state == state, f"unable to set from {slot} to {state}, current = {cur}"
+    for slot in slots:
+        for state in ["locate", "failure", "normal"]:
+            cmd.set_slot_state(slot, state)
+            cur = cmd.get_slot(slot)
+            assert cur.state == state, f"unable to set from \"{slot}\" to \"{state}\", current = \"{cur}\""

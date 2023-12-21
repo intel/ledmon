3cbc9c9a4fa69997d0d9b8b929ec72be2bc5d099
Mateusz Kusiak <96818779+mku514k@users.noreply.github.com>
2023-12-21 16:37:36 +0100

    Add yapf github action for python tests and fix python files (#197)
    Add "YAPF Python Code Formatting check" (yapf-action) github action, to
    be run on ledmon pull requests. This is to check if python test files have proper formatting.
    Fix python tests files formatting for yapf.
    
    Signed-off-by: Mateusz Kusiak <mateusz.kusiak@intel.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,14 +2,21 @@ import logging
 
 LOGGER = logging.getLogger(__name__)
 
-store_params =[
+store_params = [
     ["--ledctl-binary", "src/ledctl/ledctl", "Path to the ledctl binary."],
-    ["--slot-filters", "none", "List comma separated. Filter out slots starting with filter."],
-    ["--controller-filters", "none", "List comma separated. Filter out controllers matching by name e.g. VMD, SCSI, NPEM."]
+    [
+        "--slot-filters", "none",
+        "List comma separated. Filter out slots starting with filter."
+    ],
+    [
+        "--controller-filters", "none",
+        "List comma separated. Filter out controllers matching by name e.g. VMD, SCSI, NPEM."
+    ]
 ]
 
+
 def pytest_addoption(parser):
-     for param, def_val, help_txt in store_params:
+    for param, def_val, help_txt in store_params:
         parser.addoption(param, action="store", default=def_val, help=help_txt)
 
 
@@ -20,9 +27,9 @@ def pytest_collection_modifyitems(session, config, items):
 
 def pytest_generate_tests(metafunc):
     params = {
-        "ledctl_binary" : metafunc.config.option.ledctl_binary,
-        "slot_filters" : metafunc.config.option.slot_filters,
-        "controller_filters" : metafunc.config.option.controller_filters
+        "ledctl_binary": metafunc.config.option.ledctl_binary,
+        "slot_filters": metafunc.config.option.slot_filters,
+        "controller_filters": metafunc.config.option.controller_filters
     }
     for param, val in params.items():
         if param in metafunc.fixturenames and val is not None:

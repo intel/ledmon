83f3779a08602b2f10896eaa9963ea5183cbc5cc
Kinga Stefaniuk <kinga.tanska@intel.com>
2024-01-15 09:13:07 +0100

    Add "print" parameter to parse output from get-slot (#187)
    Add new parameter which represents chosen output
    format for get-slot command. If not set, full info
    will be printed, otherwise proper slot parameter value.
    Add new tests to parameters_test.py to check if print
    parameter is parsed properly.
    ---------
    
    Signed-off-by: Kinga Tanska <kinga.tanska@intel.com>

--- src/lib/utils.c
+++ src/lib/utils.c
@@ -585,6 +585,7 @@ struct option longopt_all[] = {
 	[OPT_DEVICE]       = {"device", required_argument, NULL, 'd'},
 	[OPT_SLOT]         = {"slot", required_argument, NULL, 'p'},
 	[OPT_STATE]        = {"state", required_argument, NULL, 's'},
+	[OPT_PRINT_PARAM]  = {"print", required_argument, NULL, 'r'},
 	[OPT_TEST]         = {"test", no_argument, NULL, 'T'},
 	[OPT_NULL_ELEMENT] = {NULL, no_argument, NULL, '\0'}
 };

7244cf0d5c84203db53992abbc7db3817b513ed8
Kinga Stefaniuk <kinga.stefaniuk@intel.com>
2024-09-25 15:48:32 +0200

    CI: do not install unnecessary packages
    Updating all of the packages every time is not needed and costs a lot of
    resources. Install only necessary packages and their dependencies.
    
    Signed-off-by: Kinga Stefaniuk <kinga.stefaniuk@intel.com>

--- .github/install_debs_compilation.sh
+++ .github/install_debs_compilation.sh
@@ -2,7 +2,7 @@
 
 # Install gcc
 if [ ! -z "$1" ]; then
-       sudo apt-get -y update && sudo apt-get -y install gcc-$1 g++-$1
+       sudo apt-get -y install gcc-$1 g++-$1 --no-upgrade --no-install-recommends --no-install-suggests
 fi
 # Install dependencies
 sudo apt-get -y install pkg-config automake autoconf autoconf-archive make libsgutils2-dev \

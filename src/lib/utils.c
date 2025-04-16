intel/ledmon

commit 94818457f615e9baa96b93d7ff5f7f5d2e5f224c

Add struct for mapping ibpi statuses to strings. (#211)
This is done different way in multiple places. Add one structure to lib
and make in commonly used. Differentiate input values and logged values,
not all patterns can be set but all patterns should be logged.

This simplifies implementation, there is no need to handle
temporary buf strings for generating "(unknown: %d)" messages.

Signed-off-by: Mariusz Tkaczyk <mariusz.tkaczyk@linux.intel.com>

src/lib/utils.c
@@ -690,49 +690,98 @@ status_t set_verbose_level(struct ledmon_conf *conf, int log_level)
 }
 
 /**
- * @brief IBPI pattern names.
- *
- * This is internal array holding names of IBPI pattern. Logging routines use
- * this entries to translate enumeration type values into the string.
+ * @brief Mapping IBPI states to strings. There are 2 strings:
+ *        - logs, uppercase for readability, should be set for all patterns.
+ *        - user input, user friendly names, could not be set.
  */
-const char *ibpi_str[LED_IBPI_PATTERN_COUNT] = {
-	[LED_IBPI_PATTERN_UNKNOWN]        = "UNKNOWN",
-	[LED_IBPI_PATTERN_NORMAL]         = "NORMAL",
-	[LED_IBPI_PATTERN_ONESHOT_NORMAL] = "ONESHOT_NORMAL",
-	[LED_IBPI_PATTERN_DEGRADED]       = "ICA",
-	[LED_IBPI_PATTERN_REBUILD]        = "REBUILD",
-	[LED_IBPI_PATTERN_FAILED_ARRAY]   = "IFA",
-	[LED_IBPI_PATTERN_HOTSPARE]       = "HOTSPARE",
-	[LED_IBPI_PATTERN_PFA]            = "PFA",
-	[LED_IBPI_PATTERN_FAILED_DRIVE]   = "FAILURE",
-	[LED_IBPI_PATTERN_LOCATE]         = "LOCATE",
-	[LED_IBPI_PATTERN_LOCATE_OFF]     = "LOCATE_OFF",
-	[LED_IBPI_PATTERN_ADDED]          = "ADDED",
-	[LED_IBPI_PATTERN_REMOVED]        = "REMOVED",
-	[LED_IBPI_PATTERN_LOCATE_AND_FAILED_DRIVE] = "LOCATE_AND_FAILURE"
+struct ibpi2names {
+	enum led_ibpi_pattern ibpi;
+	char *log_name;
+	char *input_name;
 };
 
-const char *ibpi2str_table(enum led_ibpi_pattern ibpi, const char *names[], char *buf,
-			   size_t buf_size)
+const struct ibpi2names ipbi_names[] = {
+	{LED_IBPI_PATTERN_REBUILD,		"REBUILD",		"rebuild"},
+	{LED_IBPI_PATTERN_LOCATE,		"LOCATE",		"locate"},
+	{LED_IBPI_PATTERN_LOCATE_OFF,		"LOCATE_OFF",		"locate_off"},
+	{LED_IBPI_PATTERN_LOCATE_AND_FAIL,	"LOCATE_AND_FAIL",	"locate_and_failure"},
+	{LED_SES_REQ_ABORT,			"SES_ABORT",		"ses_abort"},
+	{LED_SES_REQ_REBUILD,			"SES_REBUILD",		"ses_rebuild"},
+	{LED_SES_REQ_IFA,			"SES_IFA",		"ses_ifa"},
+	{LED_SES_REQ_ICA,			"SES_ICA",		"ses_ica"},
+	{LED_SES_REQ_CONS_CHECK,		"SES_CONS_CHECK",	"ses_cons_check"},
+	{LED_SES_REQ_HOTSPARE,			"SES_HOTSPARE",		"ses_hotspare"},
+	{LED_SES_REQ_RSVD_DEV,			"SES_RSVD_DEV",		"ses_rsvd_dev"},
+	{LED_SES_REQ_OK,			"SES_OK",		"ses_ok"},
+	{LED_SES_REQ_IDENT,			"SES_IDENT",		"ses_ident"},
+	{LED_SES_REQ_RM,			"SES_RM",		"ses_rm"},
+	{LED_SES_REQ_INS,			"SES_INSERT",		"ses_insert"},
+	{LED_SES_REQ_MISSING,			"SES_MISSING",		"ses_missing"},
+	{LED_SES_REQ_DNR,			"SES_DNR",		"ses_dnr"},
+	{LED_SES_REQ_ACTIVE,			"SES_ACTIVE",		"ses_active"},
+	{LED_SES_REQ_EN_BB,			"SES_ENABLE_BB",	"ses_enable_bb"},
+	{LED_SES_REQ_EN_BA,			"SES_ENABLE_BA",	"ses_enable_ba"},
+	{LED_SES_REQ_DEV_OFF,			"SES_DEVOFF",		"ses_devoff"},
+	{LED_SES_REQ_FAULT,			"SES_FAULT",		"ses_fault"},
+	{LED_SES_REQ_PRDFAIL,			"SES_PRDFAIL",		"ses_prdfail"},
+
+	/* Special internal patterns, can be only printed */
+	{LED_IBPI_PATTERN_UNKNOWN,		"UNKNOWN",		NULL},
+	{LED_IBPI_PATTERN_ADDED,		"ADDED",		NULL},
+	{LED_IBPI_PATTERN_REMOVED,		"REMOVED",		NULL},
+	{LED_IBPI_PATTERN_ONESHOT_NORMAL,	"ONESHOT_NORMAL",	NULL},
+
+	/* Here comes IBPI states with multiple input names defined. */
+	{LED_IBPI_PATTERN_NORMAL,	"NORMAL",	"normal"},
+	{LED_IBPI_PATTERN_NORMAL,	"NORMAL",	"off"},
+
+	{LED_IBPI_PATTERN_DEGRADED,	"ICA",		"ica"},
+	{LED_IBPI_PATTERN_DEGRADED,	"ICA",		"degraded"},
+
+	{LED_IBPI_PATTERN_FAILED_ARRAY,	"IFA",		"ifa"},
+	{LED_IBPI_PATTERN_FAILED_ARRAY,	"IFA",		"failed_array"},
+
+	{LED_IBPI_PATTERN_HOTSPARE,	"HOTSPARE",	"hotspare"},
+	{LED_IBPI_PATTERN_PFA,		"PFA",		"pfa"},
+
+	{LED_IBPI_PATTERN_FAILED_DRIVE,	"FAILURE",	"failure"},
+	{LED_IBPI_PATTERN_FAILED_DRIVE,	"FAILURE",	"disk_failed"},
+
+	{LED_IBPI_PATTERN_COUNT,	"UNKNOWN",	NULL},
+};
+
+/**
+ * @brief Return log friendly IBPI pattern name.
+ *
+ * Every led_ibpi_pattern must be defined. Do not put pointer to free.
+ */
+const char *ibpi2str(enum led_ibpi_pattern ibpi)
 {
-	const char *ret;
+	int i;
 
-	if (ibpi >= 0 && ibpi < LED_IBPI_PATTERN_COUNT)
-		ret = names[ibpi];
-	else
-		ret = NULL;
+	for (i = 0; i < ARRAY_SIZE(ipbi_names); i++)
+		if (ipbi_names[i].ibpi == ibpi)
+			return ipbi_names[i].log_name;
 
-	if (!ret) {
-		snprintf(buf, buf_size, "(unknown: %u)", ibpi);
-		ret = buf;
-	}
-
-	return ret;
+	/* That should not happen */
+	assert(false);
 }
 
-const char *ibpi2str(enum led_ibpi_pattern ibpi, char *buf, size_t buf_size)
+enum led_ibpi_pattern string2ibpi(const char *name)
 {
-	return ibpi2str_table(ibpi, ibpi_str, buf, buf_size);
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(ipbi_names); i++) {
+		char *input_name = ipbi_names[i].input_name;
+
+		if (!input_name)
+			continue;
+
+		if (strncmp(input_name, name, strlen(input_name)) == 0)
+			return ipbi_names[i].ibpi;
+	}
+
+	return LED_IBPI_PATTERN_UNKNOWN;
 }
 
 static const struct ibpi2value *get_ibpi2value(const enum led_ibpi_pattern val,

src/ledctl/ledctl.c
@@ -299,75 +299,12 @@ static struct ibpi_state *_ibpi_find(const struct list *ibpi_local_list,
  */
 static struct ibpi_state *_ibpi_state_get(const char *name)
 {
+	enum led_ibpi_pattern ibpi = string2ibpi(name);
 	struct ibpi_state *state = NULL;
-	enum led_ibpi_pattern ibpi;
 
-	if (strcmp(name, "locate") == 0) {
-		ibpi = LED_IBPI_PATTERN_LOCATE;
-	} else if (strcmp(name, "locate_off") == 0) {
-		ibpi = LED_IBPI_PATTERN_LOCATE_OFF;
-	} else if (strcmp(name, "normal") == 0) {
-		ibpi = LED_IBPI_PATTERN_NORMAL;
-	} else if (strcmp(name, "off") == 0) {
-		ibpi = LED_IBPI_PATTERN_NORMAL;
-	} else if ((strcmp(name, "ica") == 0) ||
-		   (strcmp(name, "degraded") == 0)) {
-		ibpi = LED_IBPI_PATTERN_DEGRADED;
-	} else if (strcmp(name, "rebuild") == 0) {
-		ibpi = LED_IBPI_PATTERN_REBUILD;
-	} else if ((strcmp(name, "ifa") == 0) ||
-		   (strcmp(name, "failed_array") == 0)) {
-		ibpi = LED_IBPI_PATTERN_FAILED_ARRAY;
-	} else if (strcmp(name, "hotspare") == 0) {
-		ibpi = LED_IBPI_PATTERN_HOTSPARE;
-	} else if (strcmp(name, "pfa") == 0) {
-		ibpi = LED_IBPI_PATTERN_PFA;
-	} else if ((strcmp(name, "failure") == 0) ||
-		   (strcmp(name, "disk_failed") == 0)) {
-		ibpi = LED_IBPI_PATTERN_FAILED_DRIVE;
-	} else if (strcmp(name, "locate_and_failure") == 0) {
-		ibpi = LED_IBPI_PATTERN_LOCATE_AND_FAILED_DRIVE;
-	} else if (strcmp(name, "ses_abort") == 0) {
-		ibpi = LED_SES_REQ_ABORT;
-	} else if (strcmp(name, "ses_rebuild") == 0) {
-		ibpi = LED_SES_REQ_REBUILD;
-	} else if (strcmp(name, "ses_ifa") == 0) {
-		ibpi = LED_SES_REQ_IFA;
-	} else if (strcmp(name, "ses_ica") == 0) {
-		ibpi = LED_SES_REQ_ICA;
-	} else if (strcmp(name, "ses_cons_check") == 0) {
-		ibpi = LED_SES_REQ_CONS_CHECK;
-	} else if (strcmp(name, "ses_hotspare") == 0) {
-		ibpi = LED_SES_REQ_HOTSPARE;
-	} else if (strcmp(name, "ses_rsvd_dev") == 0) {
-		ibpi = LED_SES_REQ_RSVD_DEV;
-	} else if (strcmp(name, "ses_ok") == 0) {
-		ibpi = LED_SES_REQ_OK;
-	} else if (strcmp(name, "ses_ident") == 0) {
-		ibpi = LED_SES_REQ_IDENT;
-	} else if (strcmp(name, "ses_rm") == 0) {
-		ibpi = LED_SES_REQ_RM;
-	} else if (strcmp(name, "ses_insert") == 0) {
-		ibpi = LED_SES_REQ_INS;
-	} else if (strcmp(name, "ses_missing") == 0) {
-		ibpi = LED_SES_REQ_MISSING;
-	} else if (strcmp(name, "ses_dnr") == 0) {
-		ibpi = LED_SES_REQ_DNR;
-	} else if (strcmp(name, "ses_active") == 0) {
-		ibpi = LED_SES_REQ_ACTIVE;
-	} else if (strcmp(name, "ses_enable_bb") == 0) {
-		ibpi = LED_SES_REQ_EN_BB;
-	} else if (strcmp(name, "ses_enable_ba") == 0) {
-		ibpi = LED_SES_REQ_EN_BA;
-	} else if (strcmp(name, "ses_devoff") == 0) {
-		ibpi = LED_SES_REQ_DEV_OFF;
-	} else if (strcmp(name, "ses_fault") == 0) {
-		ibpi = LED_SES_REQ_FAULT;
-	} else if (strcmp(name, "ses_prdfail") == 0) {
-		ibpi = LED_SES_REQ_PRDFAIL;
-	} else {
+	if (ibpi == LED_IBPI_PATTERN_UNKNOWN)
 		return NULL;
-	}
+
 	state = _ibpi_find(&ibpi_list, ibpi);
 	if (state == NULL)
 		state = _ibpi_state_init(ibpi);
@@ -424,9 +361,7 @@ static led_status_t _ibpi_state_add_block(struct ibpi_state *state, char *name)
 			return LED_STATUS_OUT_OF_MEMORY;
 		}
 	} else {
-		char buf[IPBI2STR_BUFF_SIZE];
-		log_info("%s: %s: device already on the list.",
-			 ibpi2str(state->ibpi, buf, sizeof(buf)), path);
+		log_info("%s: %s: device already on the list.", ibpi2str(state->ibpi), path);
 	}
 	return LED_STATUS_SUCCESS;
 }
@@ -435,14 +370,11 @@ static led_status_t verify_block_lists(void)
 {
 	if (!list_is_empty(&ibpi_list)) {
 		struct ibpi_state *state;
-		char buf[IPBI2STR_BUFF_SIZE];
 
-		list_for_each(&ibpi_list, state) {
-			if (list_is_empty(&state->block_list)) {
+		list_for_each(&ibpi_list, state)
+			if (list_is_empty(&state->block_list))
 				log_warning("IBPI %s: missing block device(s)... pattern ignored.",
-						ibpi2str(state->ibpi, buf, sizeof(buf)));
-			}
-		}
+					    ibpi2str(state->ibpi));
 	} else {
 		log_error("missing operand(s)... run %s --help for details.", progname);
 		return LED_STATUS_LIST_EMPTY;
@@ -866,10 +798,9 @@ static led_status_t verify_request(struct led_ctx *ctx, struct request *req)
 
 static void print_slot(struct led_slot_list_entry *s, enum print_param to_print)
 {
-	char buf[IPBI2STR_BUFF_SIZE];
 	const char *device_name = led_slot_device(s);
 	const char *slot_id = basename(led_slot_id(s));
-	const char *ibpi_str = ibpi2str(led_slot_state(s), buf, sizeof(buf));
+	const char *ibpi_str = ibpi2str(led_slot_state(s));
 
 	if (!device_name)
 		device_name = "(empty)";
@@ -972,12 +903,11 @@ led_status_t execute_request(struct led_ctx *ctx, struct request *req)
 	case OPT_SET_SLOT:
 	{
 		led_status_t set_rc = LED_STATUS_SUCCESS;
-		char buf[IPBI2STR_BUFF_SIZE];
 
 		if (req->state != LED_IBPI_PATTERN_LOCATE_OFF
 		    && led_slot_state(slot) == req->state) {
 			log_warning("Led state: %s is already set for the slot.",
-				    ibpi2str(req->state, buf, sizeof(buf)));
+				    ibpi2str(req->state));
 		} else {
 			set_rc = led_slot_set(ctx, slot, req->state);
 		}

src/ledmon/ledmon.c
@@ -114,28 +114,6 @@ static char *ledmon_conf_path;
 static int foreground;
 
 /**
- * @brief Name of IBPI patterns.
- *
- * This is internal array with names of IBPI patterns. Logging routines use this
- * entries to translate enumeration type into the string.
- */
-const char *ibpi_str_ledmon[] = {
-	[LED_IBPI_PATTERN_UNKNOWN]        = "None",
-	[LED_IBPI_PATTERN_NORMAL]         = "Off",
-	[LED_IBPI_PATTERN_ONESHOT_NORMAL] = "Oneshot Off",
-	[LED_IBPI_PATTERN_DEGRADED]       = "In a Critical Array",
-	[LED_IBPI_PATTERN_REBUILD]        = "Rebuild",
-	[LED_IBPI_PATTERN_FAILED_ARRAY]   = "In a Failed Array",
-	[LED_IBPI_PATTERN_HOTSPARE]       = "Hotspare",
-	[LED_IBPI_PATTERN_PFA]            = "Predicted Failure Analysis",
-	[LED_IBPI_PATTERN_FAILED_DRIVE]   = "Failure",
-	[LED_IBPI_PATTERN_LOCATE]         = "Locate",
-	[LED_IBPI_PATTERN_LOCATE_OFF]     = "Locate Off",
-	[LED_IBPI_PATTERN_ADDED]          = "Added",
-	[LED_IBPI_PATTERN_REMOVED]        = "Removed"
-};
-
-/**
  * Internal variable of monitor service. It is the pattern used to print out
  * information about the version of monitor service.
  */
@@ -677,7 +655,6 @@ static void _handle_fail_state(struct block_device *block,
 static void _add_block(struct block_device *block)
 {
 	struct block_device *temp = NULL;
-	char buf[IPBI2STR_BUFF_SIZE];
 
 	list_for_each(&ledmon_block_list, temp) {
 		if (block_compare(temp, block))
@@ -712,12 +689,10 @@ static void _add_block(struct block_device *block)
 
 		_handle_fail_state(block, temp);
 
-		if (ibpi != temp->ibpi && ibpi <= LED_IBPI_PATTERN_REMOVED) {
-			log_info("CHANGE %s: from '%s' to '%s'.",
-				 temp->sysfs_path,
-				 ibpi2str_table(ibpi, ibpi_str_ledmon, buf, sizeof(buf)),
-				 ibpi2str_table(temp->ibpi, ibpi_str_ledmon, buf, sizeof(buf)));
-		}
+		if (ibpi != temp->ibpi && ibpi <= LED_IBPI_PATTERN_REMOVED)
+			log_info("CHANGE %s: from '%s' to '%s'", temp->sysfs_path, ibpi2str(ibpi),
+				 ibpi2str(temp->ibpi));
+
 		/* Check if name of the device changed.*/
 		if (strcmp(temp->sysfs_path, block->sysfs_path)) {
 			log_info("NAME CHANGED %s to %s",
@@ -733,8 +708,8 @@ static void _add_block(struct block_device *block)
 		/* Device not found, it's a new one! */
 		temp = block_device_duplicate(block);
 		if (temp != NULL) {
-			log_info("NEW %s: state '%s'.", temp->sysfs_path,
-				 ibpi2str_table(temp->ibpi, ibpi_str_ledmon, buf, sizeof(buf)));
+			log_info("NEW %s: state '%s'.", temp->sysfs_path, ibpi2str(temp->ibpi));
+
 			if (!list_append(&ledmon_block_list, temp)) {
 				log_error("Memory allocation error!");
 				EXIT(1);
@@ -760,7 +735,6 @@ static void _add_block(struct block_device *block)
  */
 static void _send_msg(struct block_device *block)
 {
-	char buf[IPBI2STR_BUFF_SIZE];
 	if (!block->cntrl) {
 		log_debug("Missing cntrl for dev: %s. Not sending anything.",
 			  strstr(block->sysfs_path, "host"));
@@ -769,11 +743,9 @@ static void _send_msg(struct block_device *block)
 	if (block->timestamp != timestamp ||
 	    block->ibpi == LED_IBPI_PATTERN_REMOVED) {
 		if (block->ibpi != LED_IBPI_PATTERN_FAILED_DRIVE) {
-			log_info("CHANGE %s: from '%s' to '%s'.",
-				 block->sysfs_path,
-				 ibpi2str_table(block->ibpi, ibpi_str_ledmon, buf, sizeof(buf)),
-				 ibpi2str_table(LED_IBPI_PATTERN_FAILED_DRIVE, ibpi_str_ledmon,
-						buf, sizeof(buf)));
+			log_info("CHANGE %s: from '%s' to '%s'.", block->sysfs_path,
+				 ibpi2str(block->ibpi), ibpi2str(LED_IBPI_PATTERN_FAILED_DRIVE));
+
 			block->ibpi = LED_IBPI_PATTERN_FAILED_DRIVE;
 		} else {
 			char *host = strstr(block->sysfs_path, "host");

src/lib/ahci.c
@@ -88,11 +88,9 @@ int ahci_sgpio_write(struct block_device *device, enum led_ibpi_pattern ibpi)
 	ibpi2val = get_by_ibpi(ibpi, ibpi2sgpio, ARRAY_SIZE(ibpi2sgpio));
 
 	if (ibpi2val->ibpi == LED_IBPI_PATTERN_UNKNOWN) {
-		char buf[IPBI2STR_BUFF_SIZE];
-
 		lib_log(device->cntrl->ctx, LED_LOG_LEVEL_INFO,
-			"AHCI: Controller doesn't support %s pattern\n",
-			ibpi2str(ibpi, buf, sizeof(buf)));
+			"AHCI: Controller doesn't support %s pattern\n", ibpi2str(ibpi));
+
 		__set_errno_and_return(ERANGE);
 	}
 

src/lib/amd_ipmi.c
@@ -369,19 +369,17 @@ static int _enable_smbus_control(struct amd_drive *drive)
 
 static int _change_ibpi_state(struct amd_drive *drive, enum led_ibpi_pattern ibpi, bool enable)
 {
-	char buf[IPBI2STR_BUFF_SIZE];
 	const struct ibpi2value *ibpi2val = get_by_ibpi(ibpi, ibpi2amd_ipmi,
 							ARRAY_SIZE(ibpi2amd_ipmi));
 
 	if (ibpi2val->ibpi == LED_IBPI_PATTERN_UNKNOWN) {
 		lib_log(drive->ctx, LED_LOG_LEVEL_INFO,
-			"AMD_IPMI: Controller doesn't support %s pattern\n",
-			ibpi2str(ibpi, buf, sizeof(buf)));
+			"AMD_IPMI: Controller doesn't support %s pattern\n", ibpi2str(ibpi));
 		return LED_STATUS_INVALID_STATE;
 	}
 
-	lib_log(drive->ctx, LED_LOG_LEVEL_DEBUG, "%s %s LED\n",
-		(enable) ? "Enabling":"Disabling", ibpi2str(ibpi, buf, sizeof(buf)));
+	lib_log(drive->ctx, LED_LOG_LEVEL_DEBUG, "%s %s LED\n", (enable) ? "Enabling" : "Disabling",
+		ibpi2str(ibpi));
 
 	return _set_ipmi_register(enable, ibpi2val->value, drive);
 }
@@ -442,14 +440,12 @@ int _amd_ipmi_write(struct block_device *device, enum led_ibpi_pattern ibpi)
 {
 	int rc;
 	struct amd_drive drive;
-	char buf[IPBI2STR_BUFF_SIZE];
 
 	memset(&drive, 0, sizeof(struct amd_drive));
 	drive.ctx = device->cntrl->ctx;
 
 	lib_log(drive.ctx, LED_LOG_LEVEL_INFO, "\n");
-	lib_log(drive.ctx, LED_LOG_LEVEL_INFO, "Setting %s...",
-		ibpi2str(ibpi, buf, sizeof(buf)));
+	lib_log(drive.ctx, LED_LOG_LEVEL_INFO, "Setting %s...", ibpi2str(ibpi));
 
 	rc = _get_amd_ipmi_drive(device->cntrl_path, &drive);
 	if (rc)

src/lib/amd_sgpio.c
@@ -624,14 +624,12 @@ static int _set_ibpi(struct block_device *device, enum led_ibpi_pattern ibpi)
 	struct transmit_register tx_reg;
 	struct cache_entry *cache;
 	struct cache_entry cache_dup;
-	char buf[IPBI2STR_BUFF_SIZE];
 
 	memset(&drive, 0, sizeof(struct amd_drive));
 	drive.ctx = device->cntrl->ctx;
 
 	lib_log(drive.ctx, LED_LOG_LEVEL_INFO, "\n");
-	lib_log(drive.ctx, LED_LOG_LEVEL_INFO, "Setting %s...",
-		ibpi2str(ibpi, buf, sizeof(buf)));
+	lib_log(drive.ctx, LED_LOG_LEVEL_INFO, "Setting %s...", ibpi2str(ibpi));
 	lib_log(drive.ctx, LED_LOG_LEVEL_DEBUG, "\tdevice: ...%s",
 		strstr(device->sysfs_path, "/ata"));
 	lib_log(drive.ctx, LED_LOG_LEVEL_DEBUG, "\tbuffer: ...%s",

src/lib/dellssd.c
@@ -254,11 +254,9 @@ int dellssd_write(struct block_device *device, enum led_ibpi_pattern ibpi)
 	ibpi2val = get_by_ibpi(ibpi, ibpi2ssd, ARRAY_SIZE(ibpi2ssd));
 
 	if (ibpi2val->ibpi == LED_IBPI_PATTERN_UNKNOWN) {
-		char buf[IPBI2STR_BUFF_SIZE];
 
 		lib_log(device->cntrl->ctx, LED_LOG_LEVEL_INFO,
-			"SSD: Controller doesn't support %s pattern\n",
-			ibpi2str(ibpi, buf, sizeof(buf)));
+			"SSD: Controller doesn't support %s pattern\n", ibpi2str(ibpi));
 		__set_errno_and_return(EINVAL);
 	}
 

src/lib/include/led/libled.h
@@ -224,7 +224,7 @@ enum led_ibpi_pattern {
 	LED_IBPI_PATTERN_LOCATE_OFF = 11,
 	LED_IBPI_PATTERN_ADDED = 12,
 	LED_IBPI_PATTERN_REMOVED = 13,
-	LED_IBPI_PATTERN_LOCATE_AND_FAILED_DRIVE = 14,
+	LED_IBPI_PATTERN_LOCATE_AND_FAIL = 14,
 	/*
 	 * Below are SES-2 codes. Note that by default most IBPI messages are
 	 * translated into SES when needed but SES codes can be added also.

commit 5c5b04e6c2068571c6bd19adfdcda12d618f9edc

utils: Fix string2ibpi function
string2ibpi does not compare strings correctly, the function uses strncmp,
which in case strings of different lengths, may return incorrect value
if substrings of given max length are identical. In this function whole
strings must be identical.

Fix is to change strncmp to strcmp.

Fixes intel/ledmon#259
Fixes: 94818457f615 ("Add struct for mapping ibpi statuses to strings. (#211)")
Signed-off-by: Blazej Kucman <blazej.kucman@intel.com>

src/lib/utils.c
@@ -765,7 +765,7 @@ enum led_ibpi_pattern string2ibpi(const char *name)
 		if (!input_name)
 			continue;
 
-		if (strncmp(input_name, name, strlen(input_name)) == 0)
+		if (strcmp(input_name, name) == 0)
 			return ipbi_names[i].ibpi;
 	}
 